64dfb1e85077035093288c48ea5435a620df4f8d
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2024-01-10 14:34:08 +0200

    renove console.log()

--- src/App.js
+++ src/App.js
@@ -48,7 +48,6 @@ const App = () => {
   const handleSetMarkers = useCallback(
     (coordinates) => {
       setMarkers([...markers, coordinates]);
-      console.log(markers);
     },
     [markers]
   );
